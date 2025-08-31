e963f77984cbb1c8d9c65e904f274bbecb4d26c6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-08-31 22:49:50 +0530

    Spring Boot: ECommerce: Product Service: intial setup

---
+++ src/main/java/com/example/ecommerceproduct/EcommerceproductApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceproductApplication.class, args);
+	}
+
+}
