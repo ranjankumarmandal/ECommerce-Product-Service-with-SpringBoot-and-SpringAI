a781b47916663b1759233d27ecfb44db08215528
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-08-31 23:04:56 +0530

    Spring Boot: ECommerce: Product Service: added entity layer changes

---
+++ src/main/java/com/example/ecommerceproduct/entiry/Product.java
@@ -0,0 +1,24 @@
+package com.example.ecommerceproduct.entiry;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private BigDecimal price;
+    private boolean inStock;
+}
+
+
